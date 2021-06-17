be2ea85671c590abe424a4e97dca8cd157a05f17
hi-roto <hirostudy2020@gmail.com>
2021-06-17 23:47:18 +0900

    route設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order < ApplicationRecord
     ActiveRecord::Base.transaction do
       line_foods.each do |line_food|
         line_food.update_attributes!(active: false, order: self)
-      end
+    end
       self.save!
     end
   end 
