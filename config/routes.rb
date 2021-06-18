8f1c063a10530315011edecfd8fbf92ad9f8fa71
hi-roto <hirostudy2020@gmail.com>
2021-06-18 23:28:08 +0900

    DB:seed

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :restaurants do
-        resources :foods, only: %i[index]
+        resources :foods, only: [:index]
       end
-      resources :line_foods, only: %i[index create]
+      resources :line_foods, only: [:index, :create]
       put 'line_foods/replace', to: 'line_foods#replace'
-      resources :orders, only: %i[create]
+      resources :orders, only: [:create]
     end
   end
 end
